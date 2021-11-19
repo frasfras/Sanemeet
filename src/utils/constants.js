19a1ab476cc1ed8b76ba0334ba445754439cf06f
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-19 20:54:49 +0800

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,38 @@
+export const blankClassroom = [
+  {
+    name: ' member1',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+  {
+    name: ' member2',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+  {
+    name: ' member3',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+  {
+    name: ' member4',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+  {
+    name: ' member5',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+  {
+    name: ' member6',
+    participantId: false,
+    isHandRaised: false,
+    isCalledOn: false,
+  },
+];
