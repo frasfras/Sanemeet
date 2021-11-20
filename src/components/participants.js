571af40a8e3283ce12b6d037808577c6d88fad7b
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-20 10:00:04 +0800

    Update participants.js

--- src/components/participants.js
+++ src/components/participants.js
@@ -2,12 +2,7 @@ import React, {useEffect,useState, Component } from 'react';
 import Airtable from 'airtable';
 
 
-/* import UserCard from 'components/UserCard';
-import AddUserCard from 'components/AddUserCard';
-import _ from 'lodash';
-import GenderSortDropdown from 'components/GenderSortDropdown';
-import SearchUserInput from 'components/SearchUserInput'; */
- 
+
 const base= new Airtable({apiKey:"keyjVpG4zXCD49VfC" }).base('appOE3EFDFlsHg4X6');
 
 export default class MainComponent extends Component {
@@ -16,7 +11,7 @@ export default class MainComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        eventName: 'Mimosa ',
+        eventName: 'Challenges ',
       records: [],
      
     };
@@ -53,58 +48,12 @@ export default class MainComponent extends Component {
     }); */
   }
 
-  /* removeUser(user) {
-    const users = _.without(this.state.users, user);
-    this.setState({ users });
-  }
-
-  addNewUser(user) {
-    const users = this.state.users;
-    users.push(user);
-    this.setState({ users });
-  }
-
-  toggleAddSection() {
-    this.setState({
-      isAddSectionVisible: !this.state.isAddSectionVisible
-    });
-  }
-
-  changeSelectedGender(selectedGender) {
-    this.setState({ selectedGender })
-  }
-
-  changeSelectedCountry(selectedCountry) {
-    this.setState({ selectedCountry })
-  }
-
-  filterUsers(filterBy) {
-    this.setState({ filterBy });
-  }
-
-  search(query) {
-    this.setState({ query });
-  }
-
-  searchByQuery(users, query) {
-    const newUsers = [];
-    users.forEach(user => {
-      if(
-        (user.name.toLowerCase().includes(query)) ||
-        (user.region.toLowerCase().includes(query)) ||
-        (user.email.toLowerCase().includes(query)) ||
-        (user.age == query)
-      ) {
-        newUsers.push(user);
-      }
-    });
-    return newUsers;
-  }
- */
+  
+ 
   renderNavbar = () => {
       return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Construction monitoring</a>
+        <a className="navbar-brand" href="#">Challenge monitoring</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -143,7 +92,7 @@ export default class MainComponent extends Component {
               <div className = 'col-lg-6'></div>
               <h3 className = 'float-right' >
             
-             { this.state.records.length } people assigned { this.state.eventName } projects
+             { this.state.records.length } people joined { this.state.eventName } 
              </h3>
           <div className="col">
             <div className="card-deck">
@@ -190,31 +139,3 @@ const participant = ({id,  Name,  region,email, photo,sex }) => (
   
 );
 
-const MovieCard = ({id,  Name,  region,email, photo,sex }) => (
-   /*  <div className="card">
-      <img className="card-img-top img-fluid rounded-circle hvr-grow" src={photo} alt="Movie poster" />
-      <div className="card-body">
-      <h4 className="card-title">{ Name }  <i className = ''></i> </h4>
-       
-        <p className="card-text">{email}</p>
-        <p className="card-text">
-          <small className="text-muted">{region}</small>
-        </p>
-      </div>
-    </div> */
-    // 'https://dl.airtable.com/.attachmentThumbnails/0f04dd84d03272216799a09308f3f392/39f02a8a' alt="Card image cap"/>
-
-
-    <div key = {id} className="card text-center">
-    <img className="card-img-top img-fluid rounded-circle hvr-grow" src= 'https://hpanel.hostinger.com/img/ready-website.1928ba16.svg'  ></img>  <div className="card-block">
-    <h4 className="card-title">{ Name }   </h4>
-    <p>
-        <a href={`mailto:${email}?Subject=Hello%20${Name}`} target="_top">
-        <i className = "fa fa-envelope"/> - { email }
-        </a>
-    </p>
-    <p className="card-text"><small className="text-muted"> { region }   </small> </p>
-    <i className = 'fa fa-remove fa-2x float-right hvr-grow' > </i>
-    </div>
-    </div>
-  );
