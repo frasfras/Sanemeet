6394293d8a2cf6b4703a0d962bfbe997f66097f1
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-20 11:29:30 +0800

    Add files via upload

---
+++ build/precache-manifest.0158dfa13ba3b574ebb9f8eddb1aab88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ab86502c2ecfcb8941c3b081f86598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5914ed1c78a670e1014",
+    "url": "/static/css/main.7417975d.chunk.css"
+  },
+  {
+    "revision": "3394e070fd25fe139caa",
+    "url": "/static/js/2.b50b6ad9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b50b6ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5914ed1c78a670e1014",
+    "url": "/static/js/main.9c54cac0.chunk.js"
+  },
+  {
+    "revision": "c1dd87714088628c08ab",
+    "url": "/static/js/runtime-main.7d6111cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
