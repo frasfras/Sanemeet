5b87868b979355b77c8da76e9a14303d4c94849d
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-29 10:31:58 +0800

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -280,7 +280,7 @@ class Home extends Component {
                                  <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo9"> Video challenge2 </button>
               
                                  <div id="demo9" >
-                                  <iframe title="add20" class="airtable-embed" id="ftech-iframe-13" src="https://ftech-13f5c4.ingress-erytho.easywp.com/contest/video-meeting-app/?post_type=contest&p=13&embed=1"  style={{background: "transparent", border: "1px solid #ccc", frameborder:"0",  width:"100%", height:"633px"}}></iframe> 
+                                  <iframe title="add20" class="airtable-embed" id="ftech-iframe-13" src="https://ftech-13f5c4.ingress-erytho.easywp.com/?post_type=contest&p=13&embed=1"  style={{background: "transparent", border: "1px solid #ccc", frameborder:"0",  width:"100%", height:"633px"}}></iframe> 
                                                              
                                  </div>
                               
