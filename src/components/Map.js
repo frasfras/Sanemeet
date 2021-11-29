d3d83d35a9945b91f3d35bacaed7eaf7c85d6ccf
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-29 09:01:42 +0800

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState, Component } from 'react';
+import React, { Component } from 'react';
 import Airtable from 'airtable';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
