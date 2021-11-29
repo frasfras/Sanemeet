00c02bbb09b5d2d0f69233291a4b273975d1ceb4
frasfras <65541080+frasfras@users.noreply.github.com>
2021-11-29 12:20:00 +0800

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -277,8 +277,13 @@ class Home extends Component {
                                   <iframe title="add2" class="airtable-embed" id="ftech-iframe-1794" src="https://ftech.azurewebsites.net/?post_type=contest&p=1794&embed=1"  style={{background: "transparent", border: "1px solid #ccc", frameborder:"0",  width:"100%", height:"633px"}}></iframe> 
                                                              
                                  </div>   
-                                 
-                              
+                                    
+                                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo9"> Video challenge2 </button>
+              
+                                 <div id="demo9" class="collapse" >
+                                  <iframe title="add20" class="airtable-embed" id="ftech-iframe-13” src="https://ftech-13f5c4.ingress-erytho.easywp.com/?post_type=contest&p=13&embed=1"  style={{background: "transparent", border: "1px solid #ccc", frameborder:"0",  width:"100%", height:"633px"}}></iframe> 
+                                                             
+                                 </div>
                                 <div className="trinity_tab_content_description">
                                       <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo11">Fitness challenge</button>
                                 </div>
