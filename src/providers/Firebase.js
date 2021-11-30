71b84c872f75bc395490911416354a20b42c61af
frasfras <65541080+frasfras@users.noreply.github.com>
2021-12-01 01:52:09 +0800

    Update Firebase.js

--- src/providers/Firebase.js
+++ src/providers/Firebase.js
@@ -4,14 +4,14 @@ import 'firebase/database';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD365RmTl2k7_7U_XppztI2r-pmxPwmeAY",
-  authDomain: "dolbyio-legwork-demo.firebaseapp.com",
-  databaseURL: "https://dolbyio-legwork-demo-default-rtdb.firebaseio.com/",
-  projectId: "dolbyio-legwork-demo",
-  storageBucket: "dolbyio-legwork-demo.appspot.com",
-  messagingSenderId: "824149573948",
-  appId: "1:824149573948:web:25f47f9cbed93f9d5b1e39",
-  measurementId: "G-CRX91V0P75"
+  apiKey: "AIzaSyBnZRMojpITk9XE7QEIi7YCeDveGtxpVyo",
+  authDomain: "dolbyio-classroom-demo-97584.firebaseapp.com",
+  databaseURL: "https://dolbyio-classroom-demo-97584-default-rtdb.firebaseio.com/",
+  projectId: "dolbyio-classroom-demo-97584",
+  storageBucket: "dolbyio-classroom-demo-97584.appspot.com",
+  messagingSenderId: "866391242955",
+  appId: "1:866391242955:web:d00ee462d91d892f94c34e",
+  measurementId: "G-YYR9BR3BWX"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
